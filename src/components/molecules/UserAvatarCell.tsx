4d6e7897c602a7e53b88eee0712c5b7cc5359de1
Brayan Yepes <abrayan.claye@gmail.com>
2025-05-07 11:26:17 -0500

    fix: fix warning about avatar image

--- src/components/molecules/UserAvatarCell.tsx
+++ src/components/molecules/UserAvatarCell.tsx
@@ -1,11 +1,8 @@
 import { Row } from "@tanstack/react-table";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Avatar, AvatarFallback } from "../ui/avatar";
 import { User } from "@/types/general.types";
 
 export const UserAvatarCell = ({ row }: { row: Row<User> }) => {
-  const avatarArray = row.getValue("avatar") as User["avatar"];
-  const avatarUrl =
-    avatarArray && avatarArray.length > 0 ? avatarArray[0].url : "";
   const firstName = row.getValue("firstName") as string;
   const lastName = row.getValue("lastName") as string;
   const initials = `${firstName?.charAt(0) || ""}${
@@ -13,10 +10,6 @@ export const UserAvatarCell = ({ row }: { row: Row<User> }) => {
   }`.toUpperCase();
   return (
     <Avatar>
-      <AvatarImage
-        src={avatarUrl || "/placeholder.svg"}
-        alt={`${firstName} ${lastName}`}
-      />
       <AvatarFallback className="bg-primary text-white">
         {initials}
       </AvatarFallback>
